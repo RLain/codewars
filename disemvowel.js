05f6d34ef748c81f42ac4b5413316bc110121b97
Rebecca Lain <rebecca.lainphotos@gmail.com>
2022-08-02 21:15:57 +0200

    Add comment

--- disemvowel.js
+++ disemvowel.js
@@ -6,6 +6,10 @@
 
 const string = "This website is for losers LOL!"
 
+/** Removes all vowels from a string
+ * @param {string} The comment being updated
+ * @returns {string} The string with the vowels removed
+ */
 const disemvowel = (string) => {
     const removeVowels = string.replace(/a|e|i|o|u/gi, ""); //Find the vowels and replace will nothing
     return removeVowels;
